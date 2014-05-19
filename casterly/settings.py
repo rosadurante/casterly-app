8e9bce3b585a0b64465e5612c63d3f3fc7257b68
Pablo Recio <pablo@potatolondon.com>
2014-05-19 17:42:26 +0200

    Not importing global_settings

--- casterly/settings.py
+++ casterly/settings.py
@@ -24,10 +24,16 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-
-from django.conf import global_settings
-TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + (
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
     "django.core.context_processors.request",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    # django-allauth context processors
     "allauth.account.context_processors.account",
     "allauth.socialaccount.context_processors.socialaccount",
 )
