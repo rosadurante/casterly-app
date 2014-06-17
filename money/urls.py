0ef62c13d22f9a9da0f8d453fffbd4ee7994a3c7
Rosa Durante <rosa.durante@yourvine.com>
2014-06-17 20:52:40 +0100

    Improvements in Transaction view

--- money/urls.py
+++ money/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import TransactionView
+from .views import TransactionListView
 
 urlpatterns = patterns('',
-    url(r'^$', TransactionView.as_view(), name="transactions_list")
+    url(r'^$', TransactionListView.as_view(), name="transactions_list")
     # url(r'^transactions/create/$', views.TransactionsCreate.as_view(), name="transactions_create"),
     # url(r'^transactions/(?P<id>\d+)/delete/$', views.TransactionsDelete.as_view(), name="transactions_delete")
 )
--- money/views.py
+++ money/views.py
@@ -1,49 +1,30 @@
 from django.core.urlresolvers import reverse_lazy
-from django.views.generic import ListView # , CreateView, DeleteView
+from django.views.generic import ListView
 from django.views.generic.edit import FormMixin
 
 from .models import Transaction
 from .forms import CreateTransactionForm
 
-# Create your views here.
 
-class FormListView(FormMixin, ListView):
-    http_method_names = [u'get', u'post']
-
-    def get_queryset(self):
-        return self.model.objects.get_query_set()
+class TransactionListView(FormMixin, ListView):
+    model = Transaction
+    form_class = CreateTransactionForm
+    template_name = 'money/transaction_list.html'
+    success_url = reverse_lazy('transactions_list')
 
     def get_context_data(self, **kwargs):
-        context = super(FormListView, self).get_context_data(**kwargs)
-
-        if 'form' not in context:
-            context['form'] = self.form
-
+        context = super(TransactionListView, self).get_context_data(**kwargs)
+        context.update({'form': self.get_form(self.get_form_class())})
         return context
 
-    def get(self, request, *args, **kwargs):
-        # Processing form
-        self.form = self.get_form(
-            self.get_form_class()
-        )
-
-        return super(FormListView, self).get(request, *args, **kwargs)
-
     def post(self, request, *args, **kwargs):
-        form = self.get_form(
-            self.get_form_class()
-        )
+        form = self.get_form(self.get_form_class())
 
         if form.is_valid():
-            form.save()
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
 
-
-class TransactionView(FormListView):
-    model = Transaction
-    form_class = CreateTransactionForm
-    template_name = 'money/transaction_list.html'
-    # TODO: find the way to use the url name here.
-    success_url = '/transactions/'
+    def form_valid(self, form):
+        form.save()
+        return super(TransactionListView, self).form_valid(form)
