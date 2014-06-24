bdcb16a0bc44b611d30698a5f731bcd5bc716e81
Rosa Durante <me@rosadurante.com>
2014-06-24 16:13:28 +0100

    Removed comments

--- money/urls.py
+++ money/urls.py
@@ -5,6 +5,4 @@ from .views import TransactionListView, AccountListView
 urlpatterns = patterns('',
 	url(r'^accounts$', AccountListView.as_view(), name="accounts_list"),
     url(r'^transactions$', TransactionListView.as_view(), name="transactions_list")
-    # url(r'^transactions/create/$', views.TransactionsCreate.as_view(), name="transactions_create"),
-    # url(r'^transactions/(?P<id>\d+)/delete/$', views.TransactionsDelete.as_view(), name="transactions_delete")
 )
