2718859e3e48afee71118dafb7f64f7299cd631c
Rosa Durante <me@rosadurante.com>
2014-06-24 11:56:31 +0100

    Removed comments

--- stats/urls.py
+++ stats/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = patterns('',
 	url(r'^dashboard$', views.DashboardView.as_view(), name="dashboard_view"),
     url(r'^compare$', views.CompareStatsView.as_view(), name="compare_view")
-    # url(r'^transactions/create/$', views.TransactionsCreate.as_view(), name="transactions_create"),
-    # url(r'^transactions/(?P<id>\d+)/delete/$', views.TransactionsDelete.as_view(), name="transactions_delete")
 )
