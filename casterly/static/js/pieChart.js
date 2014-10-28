335615dcd6f01c2356b54a0506676933fb56afdf
Rosa Durante <me@rosadurante.com>
2014-10-28 12:02:53 +0000

    PieChart refactor. better functions and behaviour: resize, update data

--- casterly/static/js/pieChart.js
+++ casterly/static/js/pieChart.js
@@ -1,159 +1,192 @@
-/* globals define */
+/*  globals define */
 
 define('pieChart', ['underscore', 'd3'], function (_, d3) {
     'use strict';
 
-    var PieChart = function (element, data, size, r, ir, textOffset) {
-            this.element = element || 'body';
-            this.data = data || [];
-            this.width = size || 500;
-            this.height = size || 500;
-            this.radio = r || ( (size * 2 / 5) || 200 );
-            this.labelSize = (this.width - this.radio) / 10 || 30;
-            this.timeAnimation = 500;
+    var PieChart = function (options) {
+        this.element = options.element || 'body';
+        this.data = options.data || [];
+        this.width = options.size || 500;
+        this.height = options.size || 500;
 
-            this.innerRadio = ir || ( (size / 10 ) || 50 );
-            this.textOffset = textOffset || 0;
-            this.colour = d3.scale.category20c();
-        },
+        this.timeAnimation = options.animationTime || 500;
+        this.colour = d3.scale.category20c();
 
-        behaviour = {
+        // Private functions
 
-            buildCenter: function () {
-                this.centerGroup = this.vis.append("svg:g")
-                    .attr("class", "center-group")
-                    .attr("transform", "translate(" + (this.width/2) + "," + (this.height/2) + ")");
+        /**
+         *
+         */
+        this._calculateSizeAttr = function () {
+            this.radio = this.width * 2/5;
+            this.labelSize = (this.width - this.radio) / 10;
+            this.legendSize = this.width * 2/5;
+            this.innerRadio = this.width / 10;
+            this.textOffset = 0;
+        }
 
-                this.whiteCircle = this.centerGroup.append("svg:circle")
-                  .attr("fill", "white")
-                  .attr("r", this.innerRadio);
+        /**
+         *
+         */
+        this._initChart = function () {
+            var self = this;
 
-                // "TOTAL" LABEL
-                this.centerGroup.append("svg:text")
-                  .attr("class", "label")
-                  .attr("dy", -15)
-                  .attr("text-anchor", "middle") // text-align: right
-                  .text("TOTAL");
+            this._calculateSizeAttr();
 
-                //TOTAL TRAFFIC VALUE
-                this.totalValue = this.centerGroup.append("svg:text")
-                  .attr("class", "total")
-                  .attr("dy", 7)
-                  .attr("text-anchor", "middle") // text-align: right
-                  .text(this.getTotal());
+            this.chart = d3.select(this.element).append('svg')
+                .attr("width", this.width)
+                .attr("height", this.height + this.legendSize);
 
-                //UNITS LABEL
-                this.centerGroup.append("svg:text")
-                  .attr("class", "units")
-                  .attr("dy", 21)
-                  .attr("text-anchor", "middle") // text-align: right
-                  .text("£");
+            this.donut = d3.layout.pie().value(function (d) { return d.value; })
+                .startAngle(-1 * Math.PI)
+                .endAngle(0);
 
-            },
+            this.arc = d3.svg.arc()
+                .innerRadius(this.innerRadio)
+                .outerRadius(this.radio);
 
-            buildArcs: function () {
-                var self = this;
-                this.arc = d3.svg.arc()
-                    .innerRadius(this.innerRadio)
-                    .outerRadius(this.radio);
+            this.arcGroup = this.chart.append("svg:g")
+                .attr("class", "arc")
+                .attr("transform", "translate(" + (this.width/2) + "," + (this.height/2) + ")");
 
-                this.donut = d3.layout.pie().value(function (d) { return d.value; })
-                    .startAngle(-1 * Math.PI)
-                    .endAngle(0);
+            this.labelGroup = this.chart.append("svg:g")
+                .attr("class", "label-group")
+                .attr("transform", "translate(" + (this.width/2) + "," + (this.height/2 + this.legendSize) + ")");
 
-                this.arcGroup = this.vis.append("svg:g")
-                    .attr("class", "arc")
-                    .attr("transform", "translate(" + (this.width/2) + "," + (this.height/2) + ")");
+            this.initAnimation = function (b) {
+                var i = d3.interpolate({startAngle: -1 * Math.PI, endAngle: -1 * Math.PI}, b);
+                return function (t) { return self.arc(i(t)); };
+            };
 
-                this.initAnimation = function (b) {
-                    var i = d3.interpolate({startAngle: -1 * Math.PI, endAngle: -1 * Math.PI}, b);
-                    return function (t) { return self.arc(i(t)); };
-                };
+            this.animatedArcWhenClick = function (arc, index) {};
 
-                this.animatedWhenClick = function (arc, index) {};
-
-                this.paths = this.arcGroup.selectAll("path").data(this.donut(this.data));
-                this.paths.enter()
-                    .append("svg:g")
-                        .attr("class", "slice")
-                    .append("svg:path")
-                        .attr("stroke", "white")
-                        .attr("stroke-width", 0.5)
-                        .attr("d", this.arc)
-                        .attr("fill", function(d, i) { return self.colour(i); })
-                        .on("click", this.animatedWhenClick)
-                        .transition()
-                            .ease("out")
-                            .duration(this.timeAnimation)
-                            .attrTween("d", this.initAnimation);
-            },
-
-            buildLabels: function () {
-                // Labels
-                this.labelGroup = this.vis.append("svg:g")
-                    .attr("class", "label-group")
-                    .attr("transform", "translate(" + (this.width/2) + "," + (this.height/2) + ")");
-
-                this.donut = d3.layout.pie().value(function (d) { return d.value; });
-
-                var self = this;
-
-                this.rectLabels = this.labelGroup.selectAll("rect").data(this.donut(this.data));
-                this.rectLabels.enter()
-                    .append("rect")
-                        .attr("width", this.radio)
-                        .attr("height", this.radio / 10)
-                        .attr("x", 10)
-                        .attr("y", function (d,i) { return ((-1 * self.radio) + (self.labelSize * i)); })
-                        .attr("fill", function (d,i) { return self.colour(i); });
-
-                this.valueLabels = this.labelGroup.selectAll("text.value").data(this.donut(this.data));
-                this.valueLabels.enter()
-                    .append("svg:text")
-                        .attr("class", "value")
-                        // .attr("transform", labelTransform)
-                        .attr("dy", function (d, i) { return (-1 * self.radio) + (i * self.labelSize) + (self.labelSize/2); })
-                        .attr("dx", this.radio / 2)
-                        .attr("fill", "white")
-                        .attr("text-anchor", function (d) { return "middle";
-                        }).text(function (d) {
-                            var percentage = (d.value / self.getTotal()) * 100;
-                            return percentage.toFixed(1) + "%  " + d.data.label;
-                        });
-            },
-
-
-            getTotal: function () { return parseFloat(_.reduce(this.data, function (memo, item) { return memo + item.value; }, 0).toFixed(2)); },
-            updateData: function (data) { this.data = data; this.refresh(); },
-
-            refresh: function () {
-                var self = this;
-                this.totalValue.text(this.getTotal());
-
-                this.paths = this.arcGroup.selectAll("path").data(this.donut(this.data));
-                this.paths.enter()
-                    .append("svg:g")
-                        .attr("class", "slice")
-                    .append("svg:path")
-                        .attr("stroke", "white")
-                        .attr("stroke-width", 0.5)
-                        .attr("d", this.arc)
-                        .attr("fill", function(d, i) { return self.colour(i); });
-
-                this.buildLabels();
-            },
-
-            init: function () {
-                this.vis = d3.select(this.element).append("svg:svg")
-                    .attr("width", this.width)
-                    .attr("height", this.height);
-
-                // this.buildCenter();
-                this.buildArcs();
-                this.buildLabels();
-            }
+            window.addEventListener('resize', function (event) {
+                var element = document.querySelectorAll(self.element).item(0);
+                if (element.clientWidth < 1024 && element.clientWidth > 300) {
+                    self.resizeChart(element.clientWidth);
+                }
+            })
         };
 
-    _.extend(PieChart.prototype, behaviour);
+        /**
+         *
+         */
+        this._buildArcs = function () {
+            var self = this;
+
+            this.donut = d3.layout.pie().value(function (d) { return d.value; })
+                .startAngle(-1 * Math.PI)
+                .endAngle(0);
+
+            this.paths = this.arcGroup.selectAll("path").data(this.donut(this.data));
+            this.paths.enter()
+                .append("svg:g")
+                    .attr("class", "slice")
+                .append("svg:path")
+                    .attr("stroke", "white")
+                    .attr("stroke-width", 0.5)
+                    .attr("d", this.arc)
+                    .attr("fill", function(d, i) { return self.colour(i); })
+                    .on("click", this.animatedWhenClick)
+                    .transition()
+                        .ease("out")
+                        .duration(this.timeAnimation)
+                        .attrTween("d", this.initAnimation);
+        };
+
+        /**
+         *
+         */
+        this._buildLegend = function () {
+            var self = this;
+
+            this.donut = d3.layout.pie().value(function (d) { return d.value; });
+
+            this.rectLabels = this.labelGroup.selectAll("rect").data(this.donut(this.data));
+            this.rectLabels.enter()
+                .append("rect")
+                    .attr("width", this.radio)
+                    .attr("height", this.radio / 10)
+                    .attr("x", 10)
+                    .attr("y", function (d,i) { return ((-1 * self.radio) + (self.labelSize * i)) - self.radio; })
+                    .attr("fill", function (d,i) { return self.colour(i); });
+
+            this.valueLabels = this.labelGroup.selectAll("text.value").data(this.donut(this.data));
+            this.valueLabels.enter()
+                .append("svg:text")
+                    .attr("class", "value")
+                    // .attr("transform", labelTransform)
+                    .attr("dy", function (d, i) { return (-1 * self.radio) + (i * self.labelSize) + (self.labelSize/2) - self.radio; })
+                    .attr("dx", this.radio / 2)
+                    .attr("fill", "white")
+                    .attr("text-anchor", function (d) { return "middle";
+                    }).text(function (d) {
+                        var percentage = (d.value / self.getTotal()) * 100;
+                        return percentage.toFixed(1) + "%  " + d.data.label;
+                    });
+        };
+
+        this.drawChart();
+    };
+
+    /**
+     *
+     */
+    PieChart.prototype.getTotal = function () {
+        return parseFloat(_.reduce(this.data, function (memo, item) { return memo + item.value; }, 0).toFixed(2));
+    };
+
+    /**
+     *
+     */
+    PieChart.prototype.drawChart = function () {
+        if (!this.chart || !this.chart.length) this._initChart();
+
+        this._buildArcs();
+        this._buildLegend();
+    };
+
+    /**
+     *
+     */
+    PieChart.prototype.updateData = function (data) {
+        this.data = data;
+
+        this.initAnimation = function (b) {
+        var i = d3.interpolate({startAngle: -1 * Math.PI, endAngle: -1 * Math.PI}, b);
+            return function (t) { return self.arc(i(t)); };
+        };
+
+        this.paths.remove();
+        this.rectLabels.remove();
+        this.valueLabels.remove();
+
+        this.drawChart();
+    };
+
+    /**
+     *
+     */
+     PieChart.prototype.resizeChart = function (size) {
+        this.width = size;
+        this.height = size;
+
+        this.initAnimation = function () {};
+
+        this._calculateSizeAttr();
+
+        this.chart.attr('width', this.width);
+        this.chart.attr('height', this.height);
+
+        this.arc = d3.svg.arc()
+            .innerRadius(this.innerRadio)
+            .outerRadius(this.radio);
+
+        this.arcGroup.attr("transform", "translate(" + (this.width/2) + "," + (this.height/2) + ")");
+        this.labelGroup.attr("transform", "translate(" + (this.width/2) + "," + (this.height/2 + this.legendSize) + ")");
+        
+        this.updateData(this.data);
+     };
+
     return PieChart;
 });
