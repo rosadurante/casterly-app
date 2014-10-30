1ee974cde859c51c208da05b8748bb12ef033444
Rosa Durante <me@rosadurante.com>
2014-10-30 20:00:08 +0000

    Preparing heroku environment/settings

--- casterly/wsgi.py
+++ casterly/wsgi.py
@@ -13,4 +13,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "casterly.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())	
